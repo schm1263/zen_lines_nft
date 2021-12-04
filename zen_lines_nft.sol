434d82de34a64a3aa293dd1c6578e31003930950
schm1263 <schm1263@umn.edu>
2021-12-03 21:59:22 -0600

    added constructor for token name and symbol and added minting function

--- zen_lines_nft.sol
+++ zen_lines_nft.sol
@@ -1,12 +1,32 @@
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contract/access/Ownable.sol";
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721Full.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 // Create an NFT contract that inherits the Ownable.sol property from open zepplin
-contract ZenLines is Ownable, ERC721 {
+contract ZenLinesToken is Ownable, ERC721Full {
+    
     // use counters.sol to set token IDs
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    using Strings for uint256;
     
+     // create a constructor to set token name and symbol
+     constructor() public ERC721Full("ZenLinesToken", "ZRT") {}
+
+    // create a function to generate the NFT
+    function createZenLines(address owner, string memory tokenURI) public returns (uint256) {
+        // increment the token id
+        _tokenIds.increment();
+        
+        // create a uint variable for the new NFT ID and set it equal to the current increment
+        uint256 newTokenId = _tokenIds.current();
+        // mint the new NFT with the owner address and ID
+        _mint(player, newTokenId);
+        // set the token URI for this NFT
+        _setTokenURI(newTokenId, tokenURI)
+        // return the token ID
+        return newTokenId;
+    }
+
 }
