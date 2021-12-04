6d19ce154bb4edf9c37d62dc059d4a6ec54c4e0e
schm1263 <schm1263@umn.edu>
2021-12-04 13:13:40 -0600

    succussfully compiled the ZenLinesToken contract

--- zen_lines_nft.sol
+++ zen_lines_nft.sol
@@ -20,13 +20,20 @@ contract ZenLinesToken is Ownable, ERC721Full {
         _tokenIds.increment();
         
         // create a uint variable for the new NFT ID and set it equal to the current increment
-        uint256 newTokenId = _tokenIds.current();
+        uint256 tokenId = _tokenIds.current();
         // mint the new NFT with the owner address and ID
-        _mint(player, newTokenId);
+        _mint(owner, newTokenId);
         // set the token URI for this NFT
-        _setTokenURI(newTokenId, tokenURI)
+        _setTokenURI(newTokenId, tokenURI);
         // return the token ID
         return newTokenId;
     }
+    // Create function to set token URI
+    function setTokenURI(unint256 tokenId, string memory _tokenURI) public {
+        // create require statement so that only the token owner can set the token URI 
+        require(
+            _isApprovedOrOwner(_msg.sender(), tokenId), "You are not the owner!");
+            _setTokenURI(tokenId, _tokenURI);
 
+    }
 }
