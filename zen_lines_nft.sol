e6a8fc3c9a6531478d5c2094bce04688bf0fda4d
schm1263 <schm1263@umn.edu>
2021-12-06 21:40:54 -0600

    updated contract and abi file

--- zen_lines_nft.sol
+++ zen_lines_nft.sol
@@ -1,39 +1,32 @@
-pragma solidity ^0.5.0;
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721Full.sol";
+pragma solidity ^0.8.0;
+// in order to use Counters.sol utility function for token IDs and the Ownable.sol access restrictor I have to use pragma solidity 0.8.0
+// using pragma 0.8.0 requires me to import the ERC721URIStorage extension in order to make use of the built in _setTokenURI function as it was moved from the ERC721.sol when pragma 0.8.0 was released
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 // Create an NFT contract that inherits the Ownable.sol property from open zepplin
-contract ZenLinesToken is Ownable, ERC721Full {
+contract ZenLinesToken is Ownable, ERC721URIStorage {
     
     // use counters.sol to set token IDs
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     
      // create a constructor to set token name and symbol
-     constructor() public ERC721Full("ZenLinesToken", "ZRT") {}
+     constructor() ERC721("ZenLinesToken", "ZRT") {}
 
     // create a function to generate the NFT
     function createZenLines(address owner, string memory tokenURI) public returns (uint256) {
         // increment the token id
         _tokenIds.increment();
-        
         // create a uint variable for the new NFT ID and set it equal to the current increment
-        uint256 tokenId = _tokenIds.current();
+        uint256 newTokenId = _tokenIds.current();
         // mint the new NFT with the owner address and ID
-        _mint(owner, newTokenId);
+        _safeMint(owner, newTokenId);
         // set the token URI for this NFT
         _setTokenURI(newTokenId, tokenURI);
         // return the token ID
         return newTokenId;
     }
-    // Create function to set token URI
-    function setTokenURI(unint256 tokenId, string memory _tokenURI) public {
-        // create require statement so that only the token owner can set the token URI 
-        require(
-            _isApprovedOrOwner(_msg.sender(), tokenId), "You are not the owner!");
-            _setTokenURI(tokenId, _tokenURI);
-
-    }
+    
 }
